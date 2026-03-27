a953edec439d4269adb3b3a7312267d96b71ee7a
emiliojuni0r <emiliohamsyahjr@gmail.com>
2026-03-27 21:45:42 +0700

    26:52 routes

---
+++ src/routes/movieRoutes.js
@@ -0,0 +1,21 @@
+import express from 'express'
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.json({ message: "get" })
+})
+
+router.post("/hello", (req, res) => {
+    res.json({ message: "post" })
+})
+
+router.put("/hello", (req, res) => {
+    res.json({ message: "put" })
+})
+
+router.delete("/hello", (req, res) => {
+    res.json({ message: "delete" })
+})
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -1,14 +1,23 @@
-const express = require('express');
+import express from 'express'
+
+// import Routes
+import movieRoutes from "./routes/movieRoutes.js"
 
 const app = express();
 
-app.get("/hello", (req, res) => {
-    res.json({ message: "Hello Wolrd" })
-})
-
+// API routes
+app.use("/movies", movieRoutes)
 const PORT = 5001;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
 
+// GET, POST, PUT, DELETE
+
+// https://localhost:5001/
+
+// AUTH - signin,signup
+// MOVIE - getting all movies
+// USER - profile
+// WATCHLIST
