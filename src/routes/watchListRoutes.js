7775cd58305b4e6a3be6f8681d7e6b6d57d2a687
emiliojuni0r <emiliohamsyahjr@gmail.com>
2026-06-01 22:19:31 +0700

    feat: add more endpoint for watchlistRoute

--- src/routes/watchListRoutes.js
+++ src/routes/watchListRoutes.js
@@ -1,11 +1,19 @@
 import express from 'express'
-import { addTowatchList } from '../controllers/watchlistController.js';
+import { addTowatchList, removeFromWatchList, updateWatchliStItem } from '../controllers/watchlistController.js';
 import { authMiddleware } from '../middleware/authMiddleware.js';
+import { validateRequest } from '../middleware/validateRequest.js';
+import { addTowatchListSchema } from '../validator/watchlistValidator.js';
 
 const router = express.Router();
 
 router.use(authMiddleware)
 
-router.post("/", addTowatchList)
+router.post("/", validateRequest(addTowatchListSchema), addTowatchList)
+// router.post("/", authMiddleware,addTowatchList)
+
+// {{ baseUrl }}/watchlist/:id
+router.put("/:id", updateWatchliStItem)
+
+router.delete("/:id", removeFromWatchList)
 
 export default router;
